293b4e72da2eae14a4a82514eb77a83d698ad7a5
xue.wang <xue.wang@crgecent.com>
2020-01-07 10:01:53 +0800

    init project

---
+++ uno-calculator/uno-calculator/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div> {{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
